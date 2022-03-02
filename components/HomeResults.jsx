83e2100f935cda27f26ba853a84546d0197a8cb6
Paul Guillermo Ottenwalder <paulguillermo19@gmail.com>
2022-03-01 20:55:59 -0400

    Update HomeResults.jsx

--- components/HomeResults.jsx
+++ components/HomeResults.jsx
@@ -55,17 +55,17 @@ export default function HomeResults() {
     return (
       <div>
         <div className="flex flex-col items-center">
-          <span className="text-sm text-gray-700 dark:text-gray-400">
+          <span className="text-sm text-gray-700 ">
             Showing{" "}
-            <span className="font-semibold text-gray-900 dark:text-white">
+            <span className="font-semibold text-gray-900 ">
               {countFrom}
             </span>{" "}
             to{" "}
-            <span className="font-semibold text-gray-900 dark:text-white">
+            <span className="font-semibold text-gray-900 ">
               {countTo}
             </span>{" "}
             of{" "}
-            <span className="font-semibold text-gray-900 dark:text-white">
+            <span className="font-semibold text-gray-900 ">
               {totalOfCharacters}
             </span>{" "}
             Entries
@@ -73,7 +73,7 @@ export default function HomeResults() {
 
           <div className="inline-flex mt-2 xs:mt-0">
             <button
-              className="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              className="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-l hover:bg-gray-900"
               onClick={() => setPage(page === 1 ? 1 : page - 1)}
             >
               <svg
@@ -91,7 +91,7 @@ export default function HomeResults() {
               Prev
             </button>
             <button
-              className="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-r border-0 border-l border-gray-700 hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+              className="inline-flex items-center py-2 px-4 text-sm font-medium text-white bg-gray-800 rounded-r border-0 border-l border-gray-700 hover:bg-gray-900"
               onClick={() =>
                 setPage(page <= totalOfPages ? page + 1 : totalOfPages)
               }
